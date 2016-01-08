7ffe66e8a5ef7e695c22f2403b151e6461641969
Vincent Rivière <Vincent.Riviere@univ-paris1.fr>
2016-01-08 16:32:25 +0100

    Faire le ménage dans Tools.getCasUid()

--- src/java/fr/univparis1/tomcatmonitor/Tools.java
+++ src/java/fr/univparis1/tomcatmonitor/Tools.java
@@ -6,7 +6,6 @@
 package fr.univparis1.tomcatmonitor;
 
 import java.lang.reflect.Method;
-import java.util.Map;
 import javax.servlet.http.HttpSession;
 import org.apache.catalina.Session;
 
@@ -16,22 +15,24 @@ public class Tools {
             HttpSession httpSession = session.getSession();
 
             // Ancien client Yale
-            String uid = (String)httpSession.getAttribute("edu.yale.its.tp.cas.client.filter.user");
-            if (uid != null)
-                return uid;
+            String yaleUser = (String)httpSession.getAttribute("edu.yale.its.tp.cas.client.filter.user");
+            if (yaleUser != null)
+                return yaleUser;
 
             // Nouveau client Jasig
             Object assertion = httpSession.getAttribute("_const_cas_assertion_");
-            if (assertion == null)
-                return null;
+            // class org.jasig.cas.client.validation.AssertionImpl
+            if (assertion != null) {
+                Method getPrincipal = assertion.getClass().getMethod("getPrincipal");
+                Object principal = getPrincipal.invoke(assertion);
+                // class org.jasig.cas.client.authentication.AttributePrincipalImpl
 
-            Method getPrincipal = assertion.getClass().getMethod("getPrincipal");
-            Object principal = getPrincipal.invoke(assertion);
+                Method getName = principal.getClass().getMethod("getName");
+                String name = (String)getName.invoke(principal);
+                return name;
+            }
 
-            Method getName = principal.getClass().getMethod("getName");
-            uid = (String)getName.invoke(principal);
-
-            return uid;
+            return null;
         }
         catch (Exception e) {
             return null;
